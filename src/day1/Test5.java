a782d18c750bd6ab3af5f107e188b53ec2635ab8
Kife22 <Kife22@mail.ru>
2024-01-09 19:26:22 +0300

    Solution task 1 day 2

---
+++ src/day1/Test5.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int i = 9;
+        int x = 1;
+        while (x <= 9) {
+            int result = x * i;
+            System.out.println( i + "*" + x + "=" + result );
+            x++;
+
+        }
+    }
+}
